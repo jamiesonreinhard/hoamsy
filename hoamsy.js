09235a2e907514271d76d4483df768db2898e0b9
Jamieson Reinhard <jamieson.reinhard2@gmail.com>
2021-02-12 19:50:23 -0700

    hoamsy coding challenge

---
+++ hoamsy.js
@@ -0,0 +1,13 @@
+let i = 1
+while (i <= 100) {
+  if(i % 15 === 0){
+    console.log("HoamsyIsCool");
+  } else if(i % 3 === 0){
+    console.log("Hoamsy");
+  } else if(i % 5 === 0){
+    console.log("IsCool");
+  } else {
+    console.log(i);
+  }
+  i++;
+}
